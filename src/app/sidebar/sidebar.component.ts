def081d18e8be3324f6a6e703ffb144f12bf3ae5
marouamds20 <marouamerdas5@gmail.com>
2023-06-20 23:37:38 +0100

    new

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -14,24 +14,24 @@ async function getRoute() {
         var user = JSON.parse(userinfo);
         if(user.role == "admin"){
             ROUTES = [
-                { path: '/product',       title: 'Produit',           icon:'nc-bag-16',     class:''  },
-                { path: '/order',         title: 'Commande',          icon:'nc-single-copy-04',     class:''  },
+                { path: '/product',       title: 'Produits',           icon:'nc-bag-16',     class:''  },
+                { path: '/order',         title: 'Commandes',          icon:'nc-delivery-fast',     class:''  },
                 { path: '/dashboard',     title: 'Tableau de bord',   icon:'nc-chart-pie-36',       class: '' },
-                { path: '/categorie',     title: 'Categorie',         icon:'nc-tile-56',       class: '' },
+                { path: '/categorie',     title: 'Categories',         icon:'nc-tile-56',       class: '' },
                 //{ path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-                { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
+                { path: '/notifications', title: 'Alertes',     icon:'nc-bell-55',    class: '' },
                 { path: '/user',          title: 'Profile',      icon:'nc-single-02',  class: '' },
-                { path: '/liste',          title: 'Utilisateurs',      icon:'nc-badge',  class: '' },
-                { path: '/alertconfig',          title: 'Alerte Config',      icon:'nc-badge',  class: '' },
+                { path: '/liste',         title: 'Utilisateurs',      icon:'nc-badge',  class: '' },
+                { path: '/alertconfig',   title: 'Configuration',      icon:'nc-settings-gear-65',  class: '' },
             ]
 
         }
         else{
             ROUTES = [
                 { path: '/product',       title: 'Produit',           icon:'nc-bag-16',     class:''  },
-                { path: '/categorie',     title: 'Categorie',         icon:'nc-tile-56',       class: '' },
+                { path: '/categorie',     title: 'Categories',         icon:'nc-tile-56',       class: '' },
                 //{ path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-                { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
+                { path: '/notifications', title: 'Alertes',     icon:'nc-bell-55',    class: '' },
                 { path: '/user',          title: 'Profile',      icon:'nc-single-02',  class: '' },
             ]
         }
